b3a556e681117266c58aba8178ab7dc647a76ff4
Tawmy <git@tawmy.dev>
2024-05-30 10:21:32 +0200

    Only get new access token if expired, retry once

--- NetStone.Api.Client/Helpers/NetStoneApiHelper.cs
+++ NetStone.Api.Client/Helpers/NetStoneApiHelper.cs
@@ -17,26 +17,17 @@ internal static class NetStoneApiHelper
     private static readonly SocketsHttpHandler Handler = new() { PooledConnectionLifetime = TimeSpan.FromMinutes(60) };
     private static readonly HttpClient HttpClient = new(Handler);
 
+    private static readonly SemaphoreSlim AccessTokenLock = new(1, 1);
+    private static AuthenticationResult? _accessToken;
+
     private static readonly JsonSerializerOptions SearchOptions = new()
         { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull };
-
-    private static async Task<string> GetAccessTokenAsync()
-    {
-        var app = ConfidentialClientApplicationBuilder
-            .Create(Configuration.AuthClientId)
-            .WithClientSecret(Configuration.AuthClientSecret)
-            .WithOidcAuthority(Configuration.AuthAuthority.ToString())
-            .Build();
-
-        var result = await app.AcquireTokenForClient(Configuration.AuthScopes).ExecuteAsync();
-        return result.AccessToken;
-    }
 
     public static async Task<T> GetAsync<T>(Uri uri)
     {
         var request = new HttpRequestMessage(HttpMethod.Get, uri);
 
-        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", await GetAccessTokenAsync());
+        request.Headers.Authorization = await GetAuthorizationHeader();
 
         return await SendAndHandleResponseAsync<T>(request);
     }
@@ -45,7 +36,7 @@ internal static class NetStoneApiHelper
     {
         var request = new HttpRequestMessage(HttpMethod.Post, uri);
 
-        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", await GetAccessTokenAsync());
+        request.Headers.Authorization = await GetAuthorizationHeader();
 
         var queryStr = JsonSerializer.Serialize(query, SearchOptions);
         request.Content = new StringContent(queryStr, Encoding.UTF8, "application/json");
@@ -55,20 +46,41 @@ internal static class NetStoneApiHelper
 
     private static async Task<T> SendAndHandleResponseAsync<T>(HttpRequestMessage request)
     {
-        var response = await HttpClient.SendAsync(request);
+        HttpResponseMessage? response = null;
 
-        if (!response.IsSuccessStatusCode)
+        for (var i = 0; i < 2; i++)
         {
-            throw response.StatusCode switch
+            response = await HttpClient.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
             {
-                HttpStatusCode.NotFound => new NotFoundException(),
-                HttpStatusCode.InternalServerError => new ApiInternalServerError(response.Content.ToString() ??
-                    response.StatusCode.ToString()),
-                _ => new Exception(response.StatusCode.ToString())
-            };
+                break;
+            }
+
+            if (response.StatusCode == HttpStatusCode.Unauthorized && i == 0)
+            {
+                // access token was revoked. get a new one and try again.
+                request.Headers.Authorization = await GetAuthorizationHeader(true);
+            }
+            else
+            {
+                throw response.StatusCode switch
+                {
+                    HttpStatusCode.NotFound => new NotFoundException(),
+                    HttpStatusCode.InternalServerError => new ApiInternalServerError(response.Content.ToString() ??
+                        response.StatusCode.ToString()),
+                    _ => new Exception(response.StatusCode.ToString())
+                };
+            }
+        }
+
+        if (response is null)
+        {
+            throw new Exception(response?.StatusCode.ToString() ?? "API Response was null.");
         }
 
         var content = await response.Content.ReadFromJsonAsync<T>();
+        response.Dispose();
 
         if (content is null)
         {
@@ -76,5 +88,36 @@ internal static class NetStoneApiHelper
         }
 
         return content;
+    }
+
+    private static async Task<AuthenticationHeaderValue> GetAuthorizationHeader(bool forceRefresh = false)
+    {
+        await AccessTokenLock.WaitAsync();
+        try
+        {
+            if (_accessToken is null || _accessToken.ExpiresOn.UtcDateTime < DateTime.UtcNow.AddSeconds(-30) ||
+                forceRefresh)
+            {
+                // get new access token if none exists, if it's expired, or if it expires within the next 30 seconds
+                _accessToken = await GetNewAccessTokenAsync();
+            }
+        }
+        finally
+        {
+            AccessTokenLock.Release();
+        }
+
+        return new AuthenticationHeaderValue("Bearer", _accessToken.AccessToken);
+    }
+
+    private static async Task<AuthenticationResult> GetNewAccessTokenAsync()
+    {
+        var app = ConfidentialClientApplicationBuilder
+            .Create(Configuration.AuthClientId)
+            .WithClientSecret(Configuration.AuthClientSecret)
+            .WithOidcAuthority(Configuration.AuthAuthority.ToString())
+            .Build();
+
+        return await app.AcquireTokenForClient(Configuration.AuthScopes).ExecuteAsync();
     }
 }
